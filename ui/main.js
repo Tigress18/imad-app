427182e519f8a4ebc746685e77d71a07e36a7355
Subbulakshmi <30291906+Tigress18@users.noreply.github.com>
2018-04-23 14:00:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 console.log('Loaded!');
 
 
+var img = document.getElementbyId('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onClick=function(){
+    var interval = setInterval(moveRight,100);
+}
